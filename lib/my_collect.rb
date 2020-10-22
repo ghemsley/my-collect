144df07561b31f269117227dc2461104f048a614
ghemsley <ghemsley@protonmail.ch>
2020-10-22 11:45:43 -0500

    commit tree 98f5a5881ee3ee32c5c22208aeedfc5e6f750040

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(array)
+  index = 0
+  new_array = []
+  while index < array.length
+    element = yield(array[index])
+    new_array.push(element)
+    index += 1
+  end
+  return new_array
+end
